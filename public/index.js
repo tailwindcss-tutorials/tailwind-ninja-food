423228e0b3bc95ff3196abc5ce1d2f9d8570e73f
foti <footios76@gmail.com>
2022-09-23 23:13:09 +0300

    16 & 17 Responsive nav

---
+++ public/index.js
@@ -0,0 +1,8 @@
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", () => {
+  if (menu.classList.contains("hidden")) menu.classList.remove("hidden");
+  else menu.classList.add("hidden");
+  console.log("click", menu.classList);
+});
